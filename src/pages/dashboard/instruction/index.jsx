bb871c7e7193ce0999f1c34492955fb59ad0460e
黄云飞 <huangyunfeihufe@hotmail.com>
2021-07-30 17:42:29 +0800

    docs：add cli use document

--- src/pages/dashboard/instruction/index.jsx
+++ src/pages/dashboard/instruction/index.jsx
@@ -4,7 +4,7 @@ import { PageContainer } from '@ant-design/pro-layout';
 const Instruction = () => {
   return (
     <PageContainer>
-      <iframe src="/winex-proxy-docs/" frameBorder="0" width="100%" height="640px"></iframe>
+      <iframe src="/winex-proxy-docs/?time=20210730" frameBorder="0" width="100%" height="640px"></iframe>
     </PageContainer>
   );
 };
